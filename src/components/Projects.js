b36b50d8fe23e98112fc00b2f648849f13007b46
Brock Jameson <Bjameson72@gmail.com>
2019-11-21 23:00:55 -0800

    project containers added, just need photoshop work done to complete projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "../styling/Projects.css";
+import laptop from "../assets/images/laptop copy.png";
 
 class Projects extends React.Component {
   render() {
     return (
       <div id="ProjectsContainer">
         <h1 id="ProjectsTitle">Projects</h1>
+        <div id="LaptopContainer">
+          <img className="laptop" src={laptop} />
+          <img className="laptop" src={laptop} />
+        </div>
+        <div id="ButtonContainer">
+          <button id="ProjectsButton">View more work!</button>
+        </div>
       </div>
     );
   }
